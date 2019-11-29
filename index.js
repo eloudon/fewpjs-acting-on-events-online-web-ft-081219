d4930a67bac3e0a60ba1ba56cb1718d87feb4aad
Eric Loudon <loudon.eric@gmail.com>
2019-11-29 12:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
+var dodger = document.getElementById('dodger')
+
 function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+   var leftNumbers = dodger.style.left.replace('px', '')
+   var left = parseInt(leftNumbers, 10)
 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
-  }
-}
+   if (left > 0) {
+     dodger.style.left = `${left - 1}px`
+   }
+ }
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-});
+ function moveDodgerRight() {
+   var leftNumbers = dodger.style.left.replace('px', '')
+   var left = parseInt(leftNumbers, 10)
 
-function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace("px", "")
-  var right = parseInt(rightNumbers, 10)
+   if (left > 0 ) {
+     dodger.style.left = `${left + 1}px`
+   }
+ }
 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
-  }
-}
+ document.addEventListener('keydown', function(e) {
+   if (e.which === 37) {
+     moveDodgerLeft()
+   }
+ })
 
-document.addEventListener("keydown", function(e) {
-  if (e.which === "ArrowRight") {
-    moveDodgerRight();
-  }
-});
+ document.addEventListener('keydown', function(e) {
+   if (e.which === 39) {
+     moveDodgerRight()
+   }
+ }) 
